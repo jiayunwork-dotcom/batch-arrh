35e02aded315e0503bc306bfda67adeac37f1db7
“jiayun” <jiayun_work@163.com>
2026-08-21 18:18:44 +0800

    fix: ValidateTemperature 不再经 dropT 丢掉 T<=0 错误

--- internal/thermo/validate.go
+++ internal/thermo/validate.go
@@ -22,7 +22,7 @@ var ErrNonPositivePreExponential = errors.New("pre-exponential factor A must be 
 // Arrhenius exponent.
 func ValidateTemperature(t float64) error {
 	if t <= 0 {
-		return commitT(ErrNonPositiveTemperature)
+		return ErrNonPositiveTemperature
 	}
 	return nil
 }
